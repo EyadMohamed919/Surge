56cc23df1b188d3fb18e80ebbb0c718f0f866908
Eyad Mahmoud <139317529+EyadMohamed919@users.noreply.github.com>
2026-04-18 22:35:03 +0200

    fixed stationary navbar

--- contact.php
+++ contact.php
@@ -12,8 +12,8 @@
     <title>Legacy Group</title>
 </head>
 <body>
+    <?php require_once __DIR__ . "/src/view/NavBarView.php"; ?>
     <section class="sub-page-header">
-        <?php require_once __DIR__ . "/src/view/NavBarView.php"; ?>
         <h1>Contact Us</h1>
     </section>
 
